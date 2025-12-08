0243825435d15ed8f8cdc3a4924d75f523d258c6
Sadek-Hossen <hossensadek726@gmail.com>
2025-12-08 21:58:53 +0600

    project complete

--- src/server.js
+++ src/server.js
@@ -1,31 +1,27 @@
+import { Router } from "express";
+import multer from "multer";
+import {
+  createLeave,
+  createUser,
+  getLeave,
+  getUser,
+  loginUser,
+  profileUpdate,
+} from "../controllers/user-controller.js";
 
+const router = Router();
 
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import userRoters from './routers/user.router.js';
-import { connectDB } from './db/db.js';
+// DiskStorage এর পরিবর্তে MemoryStorage
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-dotenv.config();
-const app = express();
-const PORT = process.env.PORT || 5000;
+router.post("/create", upload.single("profileImage"), createUser);
+router.post("/login", loginUser);
+router.get("/get/:email", getUser);
+router.post("/leaceAplication", createLeave);
+router.get("/leave/:email", getLeave);
 
-app.use(express.json());
-app.use(cors({
-     origin: "http://localhost:3000",
-  credentials: true 
-}))
+// Profile update route
+router.patch("/update/:email", upload.single("image"), profileUpdate);
 
-
-const url = process.env.MONGODB_URL || "mongodb+srv://hossensadek726_db_user:wvuWbiZstcG4C07n@cluster0.7shvrd2.mongodb.net/?appName=Cluster0";
-connectDB(url);
-
-app.use("/api/user", userRoters);
-
-app.get("/", (req, res) => {
-  res.send("HRMS Backend is running");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+export default router;
